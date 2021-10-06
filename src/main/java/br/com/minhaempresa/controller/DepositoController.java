712818668b251275cc7adbab03f6d2ff6bc84177
Jonathan Brant <jbrant1995@gmail.com>
2021-10-06 10:10:57 -0300

    Initial commit

---
+++ src/main/java/br/com/minhaempresa/controller/DepositoController.java
@@ -0,0 +1,30 @@
+package br.com.minhaempresa.controller;
+
+import br.com.minhaempresa.service.ConsultarSaldoService;
+import br.com.minhaempresa.service.DepositoService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/depositar")
+public class DepositoController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nome = req.getParameter("nome");
+        String sobrenome = req.getParameter("sobrenome");
+        double valor = Double.valueOf(req.getParameter("valor"));
+
+        DepositoService depositoService = new DepositoService();
+        double saldo = 0;
+
+        saldo = depositoService.depositar(nome, sobrenome, valor);
+
+        resp.getWriter().println("Depósito na conta do cliente " + nome + " " + sobrenome +
+                " no valor de " + valor + " feita com sucesso");
+        resp.getWriter().println("Seu saldo atualizado é de: " + saldo);
+    }
+}
